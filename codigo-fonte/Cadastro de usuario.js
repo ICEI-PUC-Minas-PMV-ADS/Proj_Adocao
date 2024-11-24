6b78ea5b0b367cf69041b7f0317ddb55b98874a1
Vitoria Vaz <vitoriavaz.61@outlook.com>
2024-11-24 17:21:00 -0300

    Update Cadastro de usuario.js

--- codigo-fonte/Cadastro de usuario.js
+++ codigo-fonte/Cadastro de usuario.js
@@ -1,19 +1,27 @@
-Karine
 function redirecionar() {
-    console.log('Função redirecionar chamada'); o
+    console.log('Função redirecionar chamada');
+
+    
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
     const confirmarSenha = document.getElementById('confirmarSenha').value;
 
-   
+    
     if (!nome || !email || !senha || !confirmarSenha) {
         alert('Por favor, preencha todos os campos.');
         return;
     }
 
+    
+    if (senha !== confirmarSenha) {
+        alert('As senhas não coincidem. Por favor, tente novamente.');
+        return;
+    }
 
- 
-    window.location.href = '2tela.html'; 
+    
+    alert('Cadastro efetuado com sucesso!');
+
+    
+    window.location.href = 'Destaques.html';
 }
-
